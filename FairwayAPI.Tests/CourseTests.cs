a627f7b54b867e114ff41fdde8957f29409684b0
ST10141612 <st10141612@vcconnect.edu.za>
2024-11-19 02:23:06 +0200

    Added Test Solution again

---
+++ FairwayAPI.Tests/CourseTests.cs
@@ -0,0 +1,34 @@
+﻿using FairwayAPI.Models.Courses;
+using FairwayAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace FairwayAPI.Tests
+{
+    public class CourseTests
+    {
+        
+        // testing to see if app is able to retrieve courses
+        [Fact]
+        public void DoesCourseExist()
+        {
+            // Arrange
+            DotNetEnv.Env.Load();
+            var connectionString = Environment.GetEnvironmentVariable("FairwayConnectionString");
+            CourseService courseService = new CourseService(connectionString);
+            string courseId = "6448275385f1efa1a8edf8e6";
+            Course retrievedCourse = new Course();
+
+            // Act
+            retrievedCourse = courseService.GetCourse(courseId);
+
+            // Assert
+            Assert.NotNull(retrievedCourse);
+
+        }
+    }
+}
